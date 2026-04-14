d165bcb33b9ae06317d5d5db3f0630ae04d5adac
jonesarn <ajones10196@icloud.com>
2026-04-14 02:51:13 -0500

    Made into function to call unto other things in the future

--- test.py
+++ test.py
@@ -1,25 +1,27 @@
 import os
 import platform
-network_base = "192.168.1."
-ip_list = []
-if platform.system() == "Windows":
+def scan_network():
+    network_base = "192.168.1."
+    ip_list = []
+    if platform.system() == "Windows":
         ping_command = "-n 1"
-else:
+    else:
         ping_command = "-c 1"
 # Notes: We are leaving that blank so we can find multiple devices on this private IP
-for i in range(74,81):
-    ip = network_base + str(i)
+    for i in range(74,81):
+        ip = network_base + str(i)
 # Doing this as we learning python
-    response = os.system(f"ping {ping_command} {ip}")
-    if response == 0 :
+        response = os.system(f"ping {ping_command} {ip}")
+        if response == 0 :
        # print(ip_list ,"is reachable")
-        ip_list.append(ip)
-        print(ip_list," are reachable")
-        parts = ip.split(".")
-        for ip in ip_list:
-              parts = ip.split(".")
+            ip_list.append(ip)
+            print(ip_list," are reachable")
+            parts = ip.split(".")
+            for ip in ip_list:
+                parts = ip.split(".")
         # delete last part of the addres so we can make a network address
         # Have to the add a zero then add that zro to our network address
+    def get_network_address(ip):
         remove_last = parts.pop()
         parts.append("0")
         network_address =".".join(parts)
